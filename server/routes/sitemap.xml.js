7e325438d56616273f9c57b0db1a12583aba3904
shiva raj badu <shivabadu5@gmail.com>
2023-09-07 23:09:04 +0545

    sitemap checking

--- server/routes/sitemap.xml.js
+++ server/routes/sitemap.xml.js
@@ -1,10 +1,25 @@
 import { SitemapStream, streamToPromise } from "sitemap";
 import { generateSitemap } from "../../utils/sitemap";
+import puppeteer from "puppeteer";
 
 export default defineEventHandler(async (event) => {
   const url = getRequestURL(event);
 
-  const links = await generateSitemap(`https://${url.host}`);
+  let links;
+  //   const links = await generateSitemap(`https://${url.host}`);
+  try {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+    await page.goto(`https://${url.host}`, { waitUntil: "networkidle0" });
+
+    links = await page.evaluate(() =>
+      Array.from(document.querySelectorAll("a"), (each) => each.href)
+    );
+
+    browser.close();
+  } catch (error) {
+    console.log("something went wrong sorry", error);
+  }
   console.log(links);
   const sitemap = new SitemapStream({
     hostname: `https://${url.host}`,
