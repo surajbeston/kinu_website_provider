5d4898a2714966649bd2469fd9a6d3099ac0675c
shiva raj badu <shivabadu5@gmail.com>
2023-09-07 22:51:24 +0545

    sitemap checking

--- server/routes/sitemap.xml.js
+++ server/routes/sitemap.xml.js
@@ -4,17 +4,23 @@ import { generateSitemap } from "../../utils/sitemap";
 export default defineEventHandler(async (event) => {
   const url = getRequestURL(event);
 
-  const links = await generateSitemap(`https://${url.host}`);
+  const links = await generateSitemap(`http://${url.host}`);
   console.log(links);
   const sitemap = new SitemapStream({
     hostname: `https://${url.host}`,
   });
 
-  for (const link of links) {
+  //   for (const link of links) {
+  if (links) {
     sitemap.write({
-      url: link,
+      url: `kinu.com.${links.length}`,
+    });
+  } else {
+    sitemap.write({
+      url: `kinu.com.${links}`,
     });
   }
+  //   }
   sitemap.end();
   return streamToPromise(sitemap);
 });
