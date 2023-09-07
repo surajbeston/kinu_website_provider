6c5b4b8e5930375181573cc25c27ead1cc5d3408
shiva raj badu <shivabadu5@gmail.com>
2023-09-07 23:12:46 +0545

    sitemap checking

--- server/routes/sitemap.xml.js
+++ server/routes/sitemap.xml.js
@@ -1,25 +1,10 @@
 import { SitemapStream, streamToPromise } from "sitemap";
 import { generateSitemap } from "../../utils/sitemap";
-import puppeteer from "puppeteer";
 
 export default defineEventHandler(async (event) => {
   const url = getRequestURL(event);
 
-  let links;
-  //   const links = await generateSitemap(`https://${url.host}`);
-  try {
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
-    await page.goto(`https://${url.host}`, { waitUntil: "networkidle0" });
-
-    links = await page.evaluate(() =>
-      Array.from(document.querySelectorAll("a"), (each) => each.href)
-    );
-
-    browser.close();
-  } catch (error) {
-    console.log("something went wrong sorry", error);
-  }
+  const links = await generateSitemap("https://kinu-women.kinu.app");
   console.log(links);
   const sitemap = new SitemapStream({
     hostname: `https://${url.host}`,
@@ -28,11 +13,11 @@ export default defineEventHandler(async (event) => {
   //   for (const link of links) {
   if (links) {
     sitemap.write({
-      url: `kinu.com.${links.length}-https://${url.host}`,
+      url: `kinu.com-${links.length} https://${url.host}`,
     });
   } else {
     sitemap.write({
-      url: `kinu.com.${links}-https://${url.host}`,
+      url: `kinu.com-${links}-https://${url.host}`,
     });
   }
   //   }
