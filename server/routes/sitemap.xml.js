d23ce8cbc39216a41ef9ae4e250dbae67ac74d6f
shiva raj badu <shivabadu5@gmail.com>
2023-09-07 22:30:53 +0545

    sitemap testing

--- server/routes/sitemap.xml.js
+++ server/routes/sitemap.xml.js
@@ -5,16 +5,19 @@ export default defineEventHandler(async (event) => {
   const url = getRequestURL(event);
 
   const links = await generateSitemap(`${url.protocol}//${url.host}`);
-
+  console.log(links);
   const sitemap = new SitemapStream({
     hostname: `${url.protocol}//${url.host}`,
   });
 
-  for (const doc of links) {
-    sitemap.write({
-      url: doc,
-    });
-  }
+  // for (const link of links) {
+  //   sitemap.write({
+  //     url: link,
+  //   });
+  // }
+  sitemap.write({
+    url: "kinu.com.np",
+  });
   sitemap.end();
   return streamToPromise(sitemap);
 });
